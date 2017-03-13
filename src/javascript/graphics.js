2b6b7e5561b20ea2e37d539f2f0202a76de7da18
vnauthor <vnauthor@users.noreply.github.com>
2017-03-13 20:33:32 +0000

    Rounded rectangle 2D path

--- src/javascript/graphics.js
+++ src/javascript/graphics.js
@@ -19,6 +19,25 @@ function createShadowHueGradient(ctx, mid_x, mid_y, width, height, hue) {
   return gradient;
 };
 
+// Stokes a rounded rectangle,
+function roundedRect(ctx, x, y, width, height, radius) {
+
+  ctx.beginPath();
+  ctx.moveTo(x, y + radius);
+  ctx.lineTo(x, y + height - radius);
+  ctx.arcTo(x, y + height, x + radius, y + height, radius);
+  ctx.lineTo(x + width - radius, y + height);
+  ctx.arcTo(x + width, y + height, x + width, y + height-radius, radius);
+  ctx.lineTo(x + width, y + radius);
+  ctx.arcTo(x + width, y, x + width - radius, y, radius);
+  ctx.lineTo(x + radius, y);
+  ctx.arcTo(x, y, x, y + radius, radius);
+  ctx.closePath();
+
+};
+
+
+
 
 // Paints a rounded rectangle filled with a radial shadow gradient.
 function roundedShadowedRect(ctx, x, y, width, height, radius, style) {
@@ -84,4 +103,4 @@ function roundedShadowedRect(ctx, x, y, width, height, radius, style) {
 
 
 
-export { roundedShadowedRect, createShadowHueGradient };
+export { roundedRect, roundedShadowedRect, createShadowHueGradient };
